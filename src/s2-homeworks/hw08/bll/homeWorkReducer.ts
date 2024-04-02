7ff2365adc229c6c039b65a85b6226ab9631e343
taras131 <mossnabitkana@gmail.com>
2024-04-02 12:16:10 +0300

    sprint_4_4_complited

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,22 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+function compareByName(a: UserType, b:UserType) {
+    return a.name.localeCompare(b.name, 'ru', { sensitivity: 'accent' });
+}
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === 'up') {
+                return state.slice().sort((a, b) => compareByName(a,b))
+            } else {
+                return state.slice().sort((a, b) => compareByName(b,a))
+            }
+            // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= 18) // need to fix
         }
         default:
             return state

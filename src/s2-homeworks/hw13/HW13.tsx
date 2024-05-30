d3b951bb1f2aa899b813512def5f48459e971378
taras131 <mossnabitkana@gmail.com>
2024-05-30 16:10:57 +0300

    hw_13_completed

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -36,11 +36,30 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
+                setText("...всё ок)")
+                setInfo("код 200 - обычно означает что скорее всего всё ок)")
                 // дописать
 
             })
             .catch((e) => {
+                console.log(e)
                 // дописать
+                if (typeof x === "boolean") {
+                    setCode('Ошибка 500!')
+                    setImage(error500)
+                    setText("эмитация ошибки на сервере")
+                    setInfo("ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)")
+                } else if (typeof x === "undefined") {
+                    setCode('Ошибка 400!')
+                    setImage(error400)
+                    setText("Ты не отправил success в body вообще!")
+                    setInfo("ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!")
+                } else if (x === null) {
+                    setCode('Error!')
+                    setImage(errorUnknown)
+                    setText("Network Error")
+                    setInfo("AxiosError")
+                }
 
             })
     }
@@ -56,7 +75,7 @@ const HW13 = () => {
                         onClick={send(true)}
                         xType={'secondary'}
                         // дописать
-
+                        disabled={info === '...loading'}
                     >
                         Send true
                     </SuperButton>
@@ -65,7 +84,7 @@ const HW13 = () => {
                         onClick={send(false)}
                         xType={'secondary'}
                         // дописать
-
+                        disabled={info === '...loading'}
                     >
                         Send false
                     </SuperButton>
@@ -74,7 +93,7 @@ const HW13 = () => {
                         onClick={send(undefined)}
                         xType={'secondary'}
                         // дописать
-
+                        disabled={info === '...loading'}
                     >
                         Send undefined
                     </SuperButton>
@@ -83,7 +102,7 @@ const HW13 = () => {
                         onClick={send(null)} // имитация запроса на не корректный адрес
                         xType={'secondary'}
                         // дописать
-
+                        disabled={info === '...loading'}
                     >
                         Send null
                     </SuperButton>
